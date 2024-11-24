250962b3acb81713a2bf284899ab02121b6cbb5c
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2024-11-24 23:55:10 +0500

    1.0.11

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -17,19 +17,20 @@ export default function Navbar() {
   
   return (
     <>
-    <header className="h-[85px] overflow-hidden flex py-5 justify-between md:flex-row flex-col sticky top-0 z-20  bg-black ">
+    <header className="h-[85px] overflow-hidden  flex py-5 justify-between md:flex-row flex-col sticky top-0 z-20  bg-black ">
       <div className='flex justify-between'>
       <div className="text-[30px] font-semibold font-sen xs:pl-10 pl-5 cursor-pointer ">
       <span className="text-[#77effc] cursor-pointer ">ABDUL</span> WASSAY
       </div>
       <button className="font-roboto  md:flex-row mt-[2px]  text-[20px] block items-center  md:hidden  lg:mr-20 md:mr-6 flex-col z-50 ">
           <div id='hamburger' onClick={()=>toggleMenu()} className={`mx-5 cursor-pointer ${menuOpen ? 'hidden':''}`}><img className='h-5' src={pic.src} alt="" /></div>
-          <div id='cross' onClick={()=>toggleMenu()} className={`mx-5 cursor-pointer ${menuOpen ? '':'hidden'}`}><img className='h-5' src={cross.src} alt="" /></div>
+          {/* <div id='cross' onClick={()=>toggleMenu()} className={`mx-5 cursor-pointer ${menuOpen ? '':'hidden'}`}><img className='h-5' src={cross.src} alt="" /></div> */}
         </button>
         </div>
-      <nav>
+      <nav className='absolute'>
        
-        <ul id='toggleMenu' className={`font-roboto md:flex-row mt-2.5  text-[20px] md:bg-black md:h-full bg-[#77effc] md:flex lg:mr-20 md:mr-6 flex-col md:w-full w-[150px] h-screen md:relative md:right-0 fixed  z-50 md:animate-none transition-right duration-300   ${menuOpen ? 'right-0':'right-[-150px]'}`}>
+        <ul id='toggleMenu' className={`font-roboto md:flex-row md:mt-2.5  text-[20px] md:bg-black md:h-full bg-[#77effc] md:flex lg:mr-20 md:mr-6 flex-col md:w-full w-[200px] h-screen md:relative md:right-0 fixed  z-50 md:animate-none transition-right duration-300 top-0  ${menuOpen ? 'right-0':'right-[-200px]'}`}>
+        <div id='cross' onClick={()=>toggleMenu()} className={`mx-5 mt-8 cursor-pointer ${menuOpen ? '':'hidden'}`}><img className='h-7' src={cross.src} alt="" /></div>
           <div><li  onClick={()=> scrollToSection('home')} className="md:mx-3 md:my-0 my-3  mx-5 cursor-pointer  hover:widthExpand100  md:hover:text-[#77effc] w-fit"><div className='hover:animate-widthExpand100 hover:border-solid md:hover:border-[#77effc] hover:border-[#ffffff] hover:border-b-[3px]'>Home</div> </li></div>
           <div><li  onClick={()=> scrollToSection('about')} className="md:mx-3 md:my-0 my-3  mx-5 cursor-pointer  hover:widthExpand100  md:hover:text-[#77effc] w-fit"><div className='hover:animate-widthExpand100 hover:border-solid md:hover:border-[#77effc] hover:border-[#ffffff] hover:border-b-[3px]'>About</div> </li></div>
           <div><li  onClick={()=> scrollToSection('skills')} className="md:mx-3 md:my-0 my-3  mx-5 cursor-pointer  hover:widthExpand100  md:hover:text-[#77effc] w-fit"><div className='hover:animate-widthExpand100 hover:border-solid md:hover:border-[#77effc] hover:border-[#ffffff] hover:border-b-[3px]'>Skill</div> </li></div>
