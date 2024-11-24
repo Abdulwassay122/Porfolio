03ec06c08ee756a78fd9578fa232e08b0cb3c3ae
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2024-11-24 22:05:19 +0500

    1.0.9

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <title>Abdul Wassay Porfolio</title>
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
         {children}
       </body>
